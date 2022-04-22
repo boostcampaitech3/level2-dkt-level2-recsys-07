4d3d8ead8328b043b3a2c5b4446d17b82fcedc77
ryujongmoon <0917jong@gmail.com>
2022-04-22 14:29:32 +0000

    [FEAT][유종문] baseline dataloader: basic split 구현

---
+++ dkt/train.py
@@ -0,0 +1,31 @@
+import os
+
+import torch
+import wandb
+from args import parse_args
+from dkt import trainer
+from dkt.dataloader import Preprocess
+from dkt.utils import setSeeds
+
+
+def main(args):
+    wandb.login()
+
+    setSeeds(42)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    args.device = device
+
+    preprocess = Preprocess(args)
+    preprocess.load_train_data(args.file_name)
+    train_data = preprocess.get_train_data()
+
+    train_data, valid_data = preprocess.split_data(train_data)
+
+    wandb.init(project="dkt", config=vars(args))
+    trainer.run(args, train_data, valid_data)
+
+
+if __name__ == "__main__":
+    args = parse_args(mode="train")
+    os.makedirs(args.model_dir, exist_ok=True)
+    main(args)
